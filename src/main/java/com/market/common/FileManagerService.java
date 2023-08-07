4dbd6355917db3f498b8c52c3b9e6f15f61ecbe0
hnique <hnique@naver.com>
2023-08-07 14:28:10 +0900

    초기 환경세팅

---
+++ src/main/java/com/market/common/FileManagerService.java
@@ -0,0 +1,68 @@
+package com.market.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileManagerService {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	// 업로드가 된 이미지가 저장될 경로(서버)
+	public static final String FILE_UPLOAD_PATH ="C:\\Users\\PC\\Desktop\\hye\\code\\Project\\market\\workspace\\images/";
+	
+	public String saveFile(String loginId, MultipartFile file) {
+		// 파일 디렉토리(폴더)
+		String directoryName = loginId + "_" + System.currentTimeMillis() + "/";
+		String filePath = FILE_UPLOAD_PATH + directoryName;
+		
+		// 폴더 생성
+		File directory = new File(filePath);
+		if (directory.mkdir() == false) {
+			// 폴더 만드는데 실패 시 이미지 경로 null로 리턴
+			return null;
+		}
+		
+		// 파일 업로드: byte 단위 업로드
+		try {
+			byte[] bytes = file.getBytes();
+			// ★★★ 한글 이름 이미지는 올릴 수 없으므로 나중에 영문자로 바꿔서 올리기
+			Path path = Paths.get(filePath + file.getOriginalFilename()); // 디렉토리 경로 + 사용자가 올린 파일명
+			Files.write(path, bytes); // 파일 업로드
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null; // 이미지 업로드 실패 시 null 리턴
+		}
+		return "/images/" + directoryName + file.getOriginalFilename();
+	}
+		
+	// 파일 삭제 메소드
+	public void deleteFile(String imagePath) {
+		Path path = Paths.get(FILE_UPLOAD_PATH + imagePath.replace("/images/", ""));
+		if (Files.exists(path)) { // 이미지가 존재하는가?
+			// 이미지 삭제
+			try {
+				Files.delete(path);
+			} catch (IOException e) {
+				logger.info("### [FileManagerService 이미지 삭제 실패] imagePath:{}", imagePath);
+			}
+
+			// 디렉토리(폴더) 삭제
+			path = path.getParent();
+			if (Files.exists(path)) {
+				try {
+					Files.delete(path);
+				} catch (IOException e) {
+					logger.info("### [FileManagerService 이미지 폴더 삭제 실패] imagePath:{}", imagePath);
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/com/market/market/MarketController.java
@@ -0,0 +1,18 @@
+package com.market.market;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/market")
+public class MarketController {
+
+	@GetMapping("/main_view")
+	public String mainView(Model model) {
+		
+		model.addAttribute("view", "market/main");
+		return "template/layout";
+	}
+}
